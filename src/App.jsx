53c1c69e124ec90f85cdcfd00c2af2484406c453
Lucas Kaiut <lucas.kaiut@gmail.com>
2023-08-31 00:55:21 -0300

    change instagram link

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <img className="w-40 h-40 rounded-full border-2 border-white" src={Logo} alt="" />
       <h1 className=" font-bold text-2xl">Doce Bistrô</h1>
       <span className=" mt-[-10px] mb-10">Trazendo alegria em forma de doces</span>
-      <Link label="Instagram" link="https://instagram.com"/>
+      <Link label="Instagram" link="https://instagram.com/doce__bistro/"/>
       <Link label="WhatsApp" link="https://wa.me/+5541988475554"/>
       <Link label="iFood" link="https://ifood.com" />
     </div>
